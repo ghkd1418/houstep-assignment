ccf80e4b9487dae31abd60650531c6ca459cce54
ghkd1418 <1004wipi@gmail.com>
2024-01-18 00:33:23 +0900

    design: BottomDrawer 컴포넌트 퍼블리싱 [#5]

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -2,18 +2,59 @@ import styled from '@emotion/styled'
 import { colors } from '@/styles/colors'
 import CustomHead from '@/components/common/CustomHead'
 import Header from '@/components/common/Header'
+import Spacing from '@/layouts/Spacing'
+import BottomDrawer from '@/components/common/BottomDrawer'
 
 export default function Order() {
   return (
     <>
       <CustomHead title={'주문하기'} />
       <Header />
-      <Container>aa</Container>
+      <Container>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+        </ul>
+        <Cart>
+          <OrderDiv>
+            <p>총 수량 : {}개</p>
+            <p>총 가격 : {}개</p>
+          </OrderDiv>
+          <OrderButton>주문하기</OrderButton>
+        </Cart>
+      </Container>
     </>
   )
 }
 
 const Container = styled.main`
-  height: 130px;
   background-color: ${colors.grey01};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Cart = styled(BottomDrawer)`
+  height: 170px;
+  padding: 27px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const OrderDiv = styled.div`
+  align-self: end;
+  font-size: 18px;
+`
+
+const OrderButton = styled.button`
+  width: 301px;
+  height: 48px;
+
+  &:hover {
+    opacity: 0.9;
+  }
 `
