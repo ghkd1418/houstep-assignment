048aa0d8da2c0a5eafabf43cfe55b8f2a41d9fc7
ghkd1418 <1004wipi@gmail.com>
2024-01-20 22:22:13 +0900

    refacotr: react-query 옵션 변경 [#5]

--- src/constants/reactQuery.ts
+++ src/constants/reactQuery.ts
@@ -1,3 +1,12 @@
+export const QUERY_CLIENT_OPTIONS = {
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 2, // 2 min
+    },
+  },
+}
+
 export const QUERY_KEY = {
   all: ['all'] as const,
   getItems: () => [...QUERY_KEY.all, 'get-itmes'] as const,
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 import DefaultLayout from '@/layouts/DefaultLayout'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { useState } from 'react'
+import { QUERY_CLIENT_OPTIONS } from '@/constants/reactQuery'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+  const [queryClient] = useState(() => new QueryClient(QUERY_CLIENT_OPTIONS))
 
   return (
     <QueryClientProvider client={queryClient}>
